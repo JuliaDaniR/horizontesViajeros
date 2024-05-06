173132efc171b699f6dcdafdf914876400c1bad1
JuliaDaniR <julidrz@hotmail.com.ar>
2024-05-06 18:46:33 -0300

    Intentando solucionar la carga de imagenes

--- src/main/java/com/empresaTurismo/agenciadeturismo/AgenciaDeTurismoApplication.java
+++ src/main/java/com/empresaTurismo/agenciadeturismo/AgenciaDeTurismoApplication.java
@@ -1,11 +1,15 @@
 package com.empresaTurismo.agenciadeturismo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgenciaDeTurismoApplication {
 
+     @Value("${file.upload-dir}")
+    private String uploadDir;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(AgenciaDeTurismoApplication.class, args);
 
